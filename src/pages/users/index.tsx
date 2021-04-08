41715a6096151c5318644bf618843bd79a2c9559
Italo Aurelio <italoaurelior@gmail.com>
2021-04-07 21:16:40 -0300

    Create pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react'
 import { Header } from "components/Header";
+import { Pagination } from 'components/Pagination';
 import { Sidebar } from 'components/Sidebar';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -52,6 +53,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
